92cfd333780401e85aa080dc1b31b4ec13a7b87c
Jonathan Arias <jonalxh@gmail.com>
2020-12-09 13:03:25 -0500

    Checkbox field fixed migrating to bootstrap4

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ class UserView(MyModelView):
     column_editable_list = ['email', 'first_name', 'last_name']
     column_searchable_list = column_editable_list
     column_exclude_list = ['password']
-    # form_excluded_columns = column_exclude_list
+    form_excluded_columns = column_exclude_list
     column_details_exclude_list = column_exclude_list
     column_filters = column_editable_list
 
@@ -109,7 +109,7 @@ admin = flask_admin.Admin(
     app,
     'My Dashboard',
     base_template='my_master.html',
-    template_mode='bootstrap3',
+    template_mode='bootstrap4',
 )
 
 # Add model views
