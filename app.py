b34d2653d6b424b3ac794f9c7d7f1cd91b37ee1d
Jonathan Arias <jonalxh@gmail.com>
2021-01-04 07:20:40 -0500

    Password field added to user create form

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ class UserView(MyModelView):
     column_editable_list = ['email', 'first_name', 'last_name']
     column_searchable_list = column_editable_list
     column_exclude_list = ['password']
-    form_excluded_columns = column_exclude_list
+    #form_excluded_columns = column_exclude_list
     column_details_exclude_list = column_exclude_list
     column_filters = column_editable_list
 
